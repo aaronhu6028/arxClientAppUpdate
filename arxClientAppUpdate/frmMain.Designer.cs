fe5d2b58b239aad3e5582b60b1413597252048c0
aaronhu <aaronhu8026@gmail.com>
2022-03-10 00:54:06 +0800

    init

---
+++ arxClientAppUpdate/frmMain.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace arxClientAppUpdater
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lbProject = new System.Windows.Forms.ListBox();
+            this.lbCurrent2 = new System.Windows.Forms.Label();
+            this.lbNew2 = new System.Windows.Forms.Label();
+            this.lbRemote = new System.Windows.Forms.Label();
+            this.lbRelease = new System.Windows.Forms.Label();
+            this.lbLocal = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.btnAllInOne = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbProject
+            // 
+            this.lbProject.FormattingEnabled = true;
+            this.lbProject.ItemHeight = 16;
+            this.lbProject.Location = new System.Drawing.Point(14, 13);
+            this.lbProject.Margin = new System.Windows.Forms.Padding(4);
+            this.lbProject.Name = "lbProject";
+            this.lbProject.Size = new System.Drawing.Size(325, 100);
+            this.lbProject.TabIndex = 1;
+            this.lbProject.SelectedIndexChanged += new System.EventHandler(this.lbProject_SelectedIndexChanged);
+            // 
+            // lbCurrent2
+            // 
+            this.lbCurrent2.AutoSize = true;
+            this.lbCurrent2.Location = new System.Drawing.Point(11, 120);
+            this.lbCurrent2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbCurrent2.Name = "lbCurrent2";
+            this.lbCurrent2.Size = new System.Drawing.Size(112, 16);
+            this.lbCurrent2.TabIndex = 2;
+            this.lbCurrent2.Text = "Release Version:";
+            // 
+            // lbNew2
+            // 
+            this.lbNew2.AutoSize = true;
+            this.lbNew2.Location = new System.Drawing.Point(11, 136);
+            this.lbNew2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbNew2.Name = "lbNew2";
+            this.lbNew2.Size = new System.Drawing.Size(111, 16);
+            this.lbNew2.TabIndex = 3;
+            this.lbNew2.Text = "Remote Dev Ver:";
+            // 
+            // lbRemote
+            // 
+            this.lbRemote.Location = new System.Drawing.Point(121, 136);
+            this.lbRemote.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbRemote.Name = "lbRemote";
+            this.lbRemote.Size = new System.Drawing.Size(120, 16);
+            this.lbRemote.TabIndex = 8;
+            // 
+            // lbRelease
+            // 
+            this.lbRelease.Location = new System.Drawing.Point(121, 120);
+            this.lbRelease.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbRelease.Name = "lbRelease";
+            this.lbRelease.Size = new System.Drawing.Size(120, 16);
+            this.lbRelease.TabIndex = 7;
+            // 
+            // lbLocal
+            // 
+            this.lbLocal.Location = new System.Drawing.Point(121, 152);
+            this.lbLocal.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbLocal.Name = "lbLocal";
+            this.lbLocal.Size = new System.Drawing.Size(120, 16);
+            this.lbLocal.TabIndex = 14;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(11, 152);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(96, 16);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Local Dev Ver:";
+            // 
+            // btnAllInOne
+            // 
+            this.btnAllInOne.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAllInOne.Location = new System.Drawing.Point(47, 176);
+            this.btnAllInOne.Name = "btnAllInOne";
+            this.btnAllInOne.Size = new System.Drawing.Size(253, 23);
+            this.btnAllInOne.TabIndex = 16;
+            this.btnAllInOne.Text = "ALL IN ONE";
+            this.btnAllInOne.UseVisualStyleBackColor = true;
+            this.btnAllInOne.Click += new System.EventHandler(this.btnAllInOne_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 211);
+            this.Controls.Add(this.btnAllInOne);
+            this.Controls.Add(this.lbLocal);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lbRemote);
+            this.Controls.Add(this.lbRelease);
+            this.Controls.Add(this.lbNew2);
+            this.Controls.Add(this.lbCurrent2);
+            this.Controls.Add(this.lbProject);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMain";
+            this.Text = "Update Maker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox lbProject;
+        private System.Windows.Forms.Label lbCurrent2;
+        private System.Windows.Forms.Label lbNew2;
+        private System.Windows.Forms.Label lbRemote;
+        private System.Windows.Forms.Label lbRelease;
+        private System.Windows.Forms.Label lbLocal;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnAllInOne;
+    }
+}
+
---
+++ arxClientAppUpdate/frmMain.cs
@@ -0,0 +1,187 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace arxClientAppUpdater
+{
+    public partial class frmMain : Form
+    {
+        const string updateJson = "update.json";
+        const string serverUri = "https://github.com/aaronhu8026/arxClientAppUpdate/raw/main/";
+
+        public frmMain()
+        {
+            InitializeComponent();
+            LoadProject();
+        }
+
+        private void LoadProject()
+        {
+            using (StreamReader r = new StreamReader(updateJson))
+            {
+                string json = r.ReadToEnd();
+                Global.listItems = JsonConvert.DeserializeObject<List<ClsProjItem>>(json);
+            }
+
+
+            lbProject.Items.Clear();
+            foreach (var item in Global.listItems)
+            {
+                lbProject.Items.Add(item.appID);
+            }
+
+            if (lbProject.Items.Count > 0)
+                lbProject.SelectedIndex = 0;
+        }
+
+        private void lbProject_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string project = lbProject.Text;
+            if (project != "")
+            {
+                UpdateProjectInfo(project);
+            }
+        }
+
+        private void UpdateProjectInfo(string project)
+        {
+            ClsProjItem item = Global.getItem(project);
+            if (item == null) return;
+
+            lbRemote.Text = item.version;
+            lbLocal.Text = GetFlutterVersion(item.projPath);
+        }
+
+        private static string GetVersion(string exename)
+        {
+            var versionInfo = FileVersionInfo.GetVersionInfo(exename);
+            Version ver = new Version(versionInfo.ProductVersion);
+            DateTime buildDateTime = new DateTime(2000, 1, 1).Add(new TimeSpan(TimeSpan.TicksPerDay * ver.Build)); // days since 1 January 2000
+            return string.Format("v{0:yy.MM.dd}.{1:00000}", buildDateTime, ver.Revision);
+        }
+
+        private void MakeProject(ClsProjItem item)
+        {
+            string project = item.appID;
+            lbLocal.Text = item.version;
+            lbRemote.Text = item.version;
+            string apkFile = Path.Combine(item.projPath, "build/app/outputs/apk/release/app-release.apk");
+            string dstFile1 = string.Format("{0}/{1}.apk", project, project);
+            string dstFile2 = string.Format("{0}/{1}_{2}.apk", project, project, item.version);
+            item.url = serverUri + dstFile2.Replace('\\', '/');
+            item.sha256 = CalcSha256(apkFile);
+
+            string jsonString = JsonConvert.SerializeObject(Global.listItems, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(updateJson, jsonString);
+        }
+
+        private string CalcSha256(string fname)
+        {
+            string sha256;
+            using (Stream s = new FileStream(fname, FileMode.Open))
+            {
+                byte[] hash = SHA256.Create().ComputeHash(s);
+                sha256 = BitConverter.ToString(hash).Replace("-", String.Empty).ToLower();
+            }
+            return sha256;
+        }
+
+        private String GetFlutterVersion(string projPath, bool inc = false)
+        {
+            string pubspec = Path.Combine(projPath, "pubspec.yaml");
+            StringBuilder sb = new StringBuilder();
+            String line;
+            String ver = "";
+            using (StreamReader sr = new StreamReader(pubspec))
+            {
+                while (!sr.EndOfStream)
+                {
+                    line = sr.ReadLine();
+                    string vv = CheckVersion(line, inc);
+                    if (vv!="")
+                    {
+                        ver = vv;
+                        if (!inc) return ver.Replace('+', '.');
+                        line = "version: " + ver; 
+                    }
+                    sb.AppendLine(line);
+                }
+            }
+            File.WriteAllText("ver.txt", sb.ToString());
+            File.Copy("ver.txt", pubspec, true);
+
+            return ver.Replace('+', '.');
+        }
+
+        private string CheckVersion(string line, bool inc=false)
+        {
+            if (line.Length < 10) return "";
+            if (line.Substring(0, 8) != "version:") return "";
+
+            string[] vers = line.Substring(8).Split(new char[] { '.', '+' });
+            int nver = 0;
+            for (int i=0; i<vers.Length; ++i)
+            {
+                nver += int.Parse(vers[i]);
+                if (i < 2) nver *= 10;
+                else if (i == 2) nver *= 100;
+            }
+
+            if (inc) nver += 1;
+            string strVer = string.Format("{0}.{1}.{2}+{3}",
+                nver / 10000, nver / 1000 % 10, nver / 100 % 10, nver % 100);
+
+            return strVer;
+        }
+
+        private void BuildAndSubmit(ClsProjItem item)
+        {
+            lbLocal.Text = item.version;
+            lbRemote.Text = item.version;
+            string curdir = Directory.GetCurrentDirectory();
+
+            string cmd = string.Format("/C echo BUILD Version={0} & cd \"{1}\" & flutter build apk & cd \"{2}\"",
+                item.version, item.projPath, curdir);
+
+            string apkFile = Path.Combine(item.projPath, "build\\app\\outputs\\apk\\release\\app-release.apk");
+            apkFile = apkFile.Replace('/', '\\');
+            string dstFile1 = string.Format("{0}\\{1}.apk", item.appID, item.appID);
+            string dstFile2 = string.Format("{0}\\{1}_{2}.apk", item.appID, item.appID, item.version);
+            string copyfile = string.Format("copy /y {0} {1} & copy /y {0} {2} ",
+                apkFile, dstFile1, dstFile2);
+
+            string comment = string.Format("-m \"{0}_{1}\"", lbProject.Text, lbRemote.Text);
+            String submit = string.Format("choice /C Y /N /D Y /T 4 & git add -A & git commit {0} & git pull & git push & pause", comment);
+
+            ProcessStartInfo Info = new ProcessStartInfo();
+            Info.Arguments = cmd + " & " + copyfile + " & " + submit;
+            Info.FileName = "cmd.exe";
+            Info.CreateNoWindow = true;
+            Process.Start(Info);
+        }
+
+        private void btnAllInOne_Click(object sender, EventArgs e)
+        {
+            string project = lbProject.Text;
+            if (project != "")
+            {
+                var item = Global.getItem(project);
+                if (item == null) return;
+
+                item.version = GetFlutterVersion(item.projPath, true);
+                MakeProject(item);
+                BuildAndSubmit(item);
+                // await Task.Delay(10000);
+                // await Task.Delay(10000);
+                // Submit(project);
+            }
+        }
+    }
+}
